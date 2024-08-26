ae621a49d56e6595113681ca7c86cfdf32ee12fb
Priscilla Guimaraes <priscilla.srg@gmail.com>
2024-08-26 23:06:58 +0100

    created method post to create course

--- crud-spring/src/main/java/com/priscilla/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/priscilla/crudspring/controller/CourseController.java
@@ -5,10 +5,14 @@ import java.util.List;
 import com.priscilla.crudspring.model.Course;
 import com.priscilla.crudspring.repository.CourseRepository;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import lombok.AllArgsConstructor;
 
@@ -23,6 +27,12 @@ public class CourseController {
     @GetMapping
     public @ResponseBody List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus (code = HttpStatus.CREATED)
+        public Course create(@RequestBody Course course) {
+        return courseRepository.save(course);  
     }
 
 }
