518c727169dcd3c4539bf70d7b73f3afa8a8f64a
Priscilla Guimaraes <priscilla.srg@gmail.com>
2024-09-11 19:51:12 +0100

    ngrx, jakarta validation, stacktrace

--- crud-spring/src/main/java/com/priscilla/crudspring/model/Course.java
+++ crud-spring/src/main/java/com/priscilla/crudspring/model/Course.java
@@ -5,6 +5,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -19,9 +24,16 @@ public class Course {
     @JsonProperty("id")
     private Long id;
     
-    @Column(length = 200, nullable = false)
+    @NotBlank
+    @NotNull
+    @Length(min = 5, max = 100 )
+    @Column(length = 100, nullable = false)
     private String name;
 
+    @NotBlank
+    @NotNull
+    @Length(max = 10 )
+    @Pattern(regexp = "Back end|Front end")
     @Column(length = 10, nullable = false)
     private String category;
 
